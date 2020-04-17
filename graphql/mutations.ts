3405e068494adb17b9721f5e679693a001602050
Jeff Barragan <leshz@me.com>
2020-04-16 21:26:05 -0500

    consulta

--- graphql/mutations.ts
+++ graphql/mutations.ts
@@ -1,6 +1,5 @@
 import { Mongo } from "../bd/db";
-import { Db } from "mongodb";
-import moduleName from "";
+import {ObjectID} from "mongodb";
 
 export const mutations = {
   createEmployee: async (root, { input }) => {
@@ -25,4 +24,34 @@ export const mutations = {
     }
     return input
   },
+  editEmployee: async (root, {employeeID, input }) => { 
+
+    let DB;
+    let employee=[];
+    let project;
+
+    try {
+      DB = await Mongo.getConnection();
+
+      // if (input.project !== undefined) {         
+      //   project = await DB.collection('projects').findOne({ _id: ObjectID(input.project) });
+      //   if (!project) throw new Error('Project doesnt exist');
+      // }   
+      // await DB.collection('employees').updateOne(
+      //   { _id: ObjectID(employeeID) },
+      //   { $set: input }
+      // )
+      // const coco = ;
+      // console.log(coco);
+      console.log(input);
+      
+      
+      employee = await DB.collection('employees').find({ _id: new ObjectID(employeeID)})     
+      
+    } catch (error) {
+      console.log(error);
+    }
+    return employee;
+    
+  }
 };
