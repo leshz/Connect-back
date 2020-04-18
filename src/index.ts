5fe6d29d0ddeeb18275c524b1ac7b85fb81cb6ac
Jeff Barragan <leshz@me.com>
2020-04-18 16:29:35 -0500

    fox cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import express = require("express");
 import gqlMiddleware from "express-graphql";
+import cors = require("cors");
 import { makeExecutableSchema } from "graphql-tools";
 import { resolvers } from "./graphql/resolvers";
 import { readFileSync } from "fs";
@@ -17,6 +18,8 @@ const typeDefs = readFileSync(
 );
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+app.use(cors())
 
 app.use(
   "/api",
